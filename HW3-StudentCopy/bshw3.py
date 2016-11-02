0fca0f50e0e9f5b49234ce2be2ccf096044f6030
Aditi Rajadhyaksha <amrajad@umich.edu>
2016-11-02 15:05:18 -0400

    BS works for the logos yay

--- HW3-StudentCopy/bshw3.py
+++ HW3-StudentCopy/bshw3.py
@@ -17,6 +17,10 @@ import requests
 import unicodedata
 from bs4 import BeautifulSoup
 
+# import HTMLParser
+# h = HTMLParser.HTMLParser()
+# h.unescape('&lt; &gt;')
+
 url = 'https://www.si.umich.edu/programs/bachelor-science-information/bsi-admissions'
 r = requests.get(url)
 soup = BeautifulSoup(r.text, "html.parser")
@@ -28,8 +32,25 @@ for student in find_student:
 
 img_tags = soup('img')
 for img in img_tags:
-	local_img = img.replace_with(str('<')+'img src ="file:///Users/AditiRajadhyaksha/Desktop/project3/HW3-StudentCopy/media/logo.png"'+str('/>'))
+	local_img = img.replace_with('<img src ="file:///Users/AditiRajadhyaksha/Desktop/project3/HW3-StudentCopy/media/logo.png"/>')
 
-print(soup.prettify())
+# # Replace '&lt;' with '<'
+# find_error = soup.find_all(text = re.compile('&lt;'))
+# for error in find_error:
+# 	fixed_error = str(error).replace('&lt;', '<')
+# 	error.replace_with(fixed_error)
+
+# #&gt;
+# find_other_error = soup.find_all(text = re.compile('&gt;'))
+# for other_error in find_other_error:
+# 	fixed_other_error = str(other_error).replace('&gt;', '>')
+# 	other_error.replace_with(fixed_error)
+
+
+print(soup.prettify(formatter=None))
+
+
+
+
 
 #class for main image: class="ytp-thumbnail-overlay ytp-cued-thumbnail-overlay"
