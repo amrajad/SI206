c0a0f869acaa5c72338f2fe9a358a94550f2f159
Aditi Rajadhyaksha <amrajad@umich.edu>
2016-11-02 15:11:47 -0400

    updating

--- HW3-StudentCopy/bshw3.py
+++ HW3-StudentCopy/bshw3.py
@@ -17,37 +17,25 @@ import requests
 import unicodedata
 from bs4 import BeautifulSoup
 
-# import HTMLParser
-# h = HTMLParser.HTMLParser()
-# h.unescape('&lt; &gt;')
-
 url = 'https://www.si.umich.edu/programs/bachelor-science-information/bsi-admissions'
 r = requests.get(url)
 soup = BeautifulSoup(r.text, "html.parser")
 
+#1
 find_student = soup.find_all(text = re.compile('student'))
 for student in find_student:
 	AMAZING_student = str(student).replace('student', 'AMAZING student')
 	student.replace_with(AMAZING_student)
 
+#2
+
+#3
 img_tags = soup('img')
 for img in img_tags:
 	local_img = img.replace_with('<img src ="file:///Users/AditiRajadhyaksha/Desktop/project3/HW3-StudentCopy/media/logo.png"/>')
 
-# # Replace '&lt;' with '<'
-# find_error = soup.find_all(text = re.compile('&lt;'))
-# for error in find_error:
-# 	fixed_error = str(error).replace('&lt;', '<')
-# 	error.replace_with(fixed_error)
 
-# #&gt;
-# find_other_error = soup.find_all(text = re.compile('&gt;'))
-# for other_error in find_other_error:
-# 	fixed_other_error = str(other_error).replace('&gt;', '>')
-# 	other_error.replace_with(fixed_error)
-
-
-print(soup.prettify(formatter=None))
+print(soup.prettify(formatter=None)) #This prints the index.html code
 
 
 
